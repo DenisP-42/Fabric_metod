5d3b311193e92ea36aa6e8bbf2539b6807a843b7
DenisP-42 <130931225+DenisP-42@users.noreply.github.com>
2023-04-16 22:40:00 +0300

    Add files via upload

---
+++ Fabric_metod/Fabric_metod/Fabric_metod/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabric_metod
+{
+    public class SquareFactory : IShapeFactory
+    {
+        public Shape CreateShape() => new Square();
+    }
+    public class LineFactory : IShapeFactory
+    {
+        public Shape CreateShape() => new Line();
+    }
+    public class CircleFactory : IShapeFactory
+    {
+        public Shape CreateShape() => new Circle();
+    }
+    public interface IShapeFactory
+    {
+        Shape CreateShape();
+    }
+    public abstract class Shape
+    {
+        public abstract void Draw(Graphics g, Point location, int h, int k);
+    }
+
+    public class Square : Shape
+    {
+        public override void Draw(Graphics g, Point location, int h, int k)
+        {
+            g.DrawRectangle(Pens.Black, location.X, location.Y, h, k);
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public override void Draw(Graphics g, Point location, int h, int k)
+        {
+            g.DrawEllipse(Pens.Black, location.X, location.Y, h, k);
+        }
+    }
+    public class Line : Shape
+    {
+        public override void Draw(Graphics g, Point location, int h, int k)
+        {
+            g.DrawLine(Pens.Black, location.X, location.Y, h, k);
+        }
+    }
+}
---
+++ Fabric_metod/Fabric_metod/Fabric_metod/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fabric_metod
+{
+    public partial class Form1 : Form
+    {
+        IShapeFactory creator;
+        public Graphics device;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+            creator = new SquareFactory();
+                Shape shape = creator.CreateShape();
+                shape.Draw(pictureBox1.CreateGraphics(), new Point(0, 0), Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+            creator = new CircleFactory();
+            Shape shape = creator.CreateShape();
+            shape.Draw(pictureBox1.CreateGraphics(), new Point(0, 0), Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+            creator = new LineFactory();
+            Shape shape = creator.CreateShape();
+            shape.Draw(pictureBox1.CreateGraphics(), new Point(0, 0), Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+        }
+    }
+}
